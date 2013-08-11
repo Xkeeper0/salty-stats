915625e21cdc312c894ba7f6993afda132fa29f1
Xkeeper <xkeeper@gmail.com>
2013-08-11 06:19:38 -0400

    Implemented a bunch of stuff all over

--- match.php
+++ match.php
@@ -11,22 +11,57 @@
 		 * Set up a new match between two characters
 		 * @param [type] $char1
 		 * @param [type] $char2
+		 * @todo  Maybe consider setting them via array
 		 */
 		public function __construct($char1, $char2) {
 			global $db;
 			$this->_db	= $db;
 
+			// Store characters
+			print "Storing characters...";
+			$this->_characters[1]	= $this->_getCharacterId($char1);
+			$this->_characters[2]	= $this->_getCharacterId($char2);
+			print " stored ". count($this->_characters) ."\n";
+
+			// Create new match row
+			$this->_createMatch();
+
 
 		}
 
 		/**
-		 * [startMatch description]
+		 * Actual match begins (fighting part), log bets
 		 * @param  [type] $data
 		 * @param  [type] $players
 		 * @return [type]
 		 */
 		public function startMatch($data, $players) {
 			// Update match data (total bet, wagers)
+
+
+			/*
+			Player data example:
+			(Shoudl probably turn off autoindex)
+			"64303":{"n":"cookiemonstar",
+					"p":"1",
+					"w":"2",
+					"b":"32",
+					"r":"1",
+					"g":"0"},
+
+			 */
+			foreach ($players as $id => $player) {
+
+				printf("[%6d] %20s - P=%2d  Wager=%6d  Total=%7d  Rank=%2d  Gold=%2d\n",
+					$id,
+					$player['n'],
+					$player['p'],
+					$player['w'],
+					$player['b'],
+					$player['r'],
+					$player['g']
+					);
+			}
 
 		}
 
@@ -39,6 +74,16 @@
 		public function completeMatch($data, $players) {
 
 		}
+
+		/**
+		 * [_updatePlayerData description]
+		 * @param  [type] $players
+		 * @return [type]
+		 */
+		protected function _updatePlayerData($players) {
+			// Loop through all players and REPLACE INTO as needed ...
+		}
+
 
 		/**
 		 * Get the character id (or create a new one)
@@ -75,6 +120,45 @@
 		}
 
 
+		/**
+		 * Create a new match row (currently blank)
+		 * @return [type]
+		 */
+		protected function _createMatch() {
+
+			$this->_db->beginTransaction();
+
+			print "Creating match\n";
+			// Creathe empty match row
+			$match		= $this->_db->query("
+					INSERT INTO	`matches` (
+						/* intentionally left blank */
+					) VALUES (
+						/* intentionally left blank */
+					)
+				");
+			$this->_matchId	= $this->_db->lastInsertId();
+
+			// Insert character_matches rows
+			$chars	= $this->_db->prepare("
+					INSERT INTO	`character_matches`
+					SET			`match_id`		= :match_id,
+								`character_id`	= :character_id,
+								`position`		= :position
+				");
+			print "Inserting characters: ". count($this->_characters) ."\n";
+			foreach ($this->_characters as $position => $characterId) {
+				print "$position: ". $this->_matchId .", ". $characterId ."\n";
+				$chars->execute(array(
+						':match_id'		=> $this->_matchId,
+						':character_id'	=> $characterId,
+						':position'		=> $position,
+					));
+			}
+
+			$this->_db->commit();
+
+		}
 
 	}
 
--- salt.php
+++ salt.php
@@ -4,6 +4,8 @@
 	include "functions.php";
 
 	$lasttime	= null;
+	$match		= null;
+
 	while (true) {
 
 		//$json	= getweb("http://saltybet.com/betdata.json");
@@ -15,6 +17,9 @@
 			$lasttime       = $json['cdata']['filetime'];
 
 			$data	= json_decode($json['data'], true);
+			$data['p1total']	= str_replace(",", "", $data['p1total']);
+			$data['p2total']	= str_replace(",", "", $data['p2total']);
+
 			print "\n";
 			printf("%30s : %8d\n%30s : %8d\n\nstatus: %s\nalert: %s\n",
 				$data['p1name'],
@@ -24,6 +29,33 @@
 				$data['status'],
 				$data['alert']
 				);
+
+			switch ($data['status']) {
+
+
+				case "open":
+					// Match starting, create bets/characters
+					$match	= new Match($data['p1name'], $data['p2name']);
+					break;
+
+				case "locked":
+					// Match started, update bets
+					if ($match) $match->startMatch($data, stripdata($data));
+					break;
+
+				case "1":
+				case "2":
+					// Match over, set winners
+					//$players	= stripdata($data);
+					//print_r($players);
+					break;
+
+
+
+			}
+
+
+
 
 		} elseif ($lasttime !== $json['cdata']['filetime']) {
 			print "X";
@@ -40,3 +72,23 @@
 #	print_r($json['cdata']);
 
 	print "\n\n";
+
+
+
+
+	/**
+	 * Remove non-player keys from array
+	 * @param  array $a
+	 * @return array
+	 */
+	function stripdata($a) {
+
+		$removekeys	= array('p1name', 'p2name', 'p1total', 'p2total', 'status', 'alert');
+		foreach ($removekeys as $key) {
+			unset($a[$key]);
+		}
+
+		return $a;
+
+
+	}
